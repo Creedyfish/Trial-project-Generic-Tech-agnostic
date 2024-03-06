ac065fcb76fc06731c72040c1e14bc03e9fbbf14
Creedyfish <ielbanbuenaofficial@gmail.com>
2024-03-07 01:29:05 +0800

    refactor:  Update header logo image

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
@@ -6,7 +7,12 @@ function Header() {
   return (
     <nav className="w-full flex items-center justify-between bg-primary py-4 px-5">
       <Link href={"/"} className="">
-        <Image src={"/cooky.png"} width={165.01} height={51.35} alt="logo" />
+        <Image
+          src={"/header-cooky-logo.png"}
+          width={165.01}
+          height={51.35}
+          alt="logo"
+        />
       </Link>
       <button>
         <Image src={"/cog.png"} width={24} height={24} alt="cog" />
