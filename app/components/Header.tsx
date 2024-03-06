a1d972a690fd411b1d8b2eaab2e98b5bd989297a
Creedyfish <ielbanbuenaofficial@gmail.com>
2024-03-06 23:28:24 +0800

    feat: Add header component to layout

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 function Header() {
-  return <nav>Header</nav>;
+  return (
+    <nav className="w-full flex items-center justify-between bg-primary py-4 px-5">
+      <Link href={"/"} className="">
+        <Image src={"/cooky.png"} width={165.01} height={51.35} alt="logo" />
+      </Link>
+      <button>
+        <Image src={"/cog.png"} width={24} height={24} alt="cog" />
+      </button>
+    </nav>
+  );
 }
 
 export default Header;
