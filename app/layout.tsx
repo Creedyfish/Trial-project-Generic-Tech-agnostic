8944752e51bc6627ea4a72acd524702ccea76191
Creedyfish <ielbanbuenaofficial@gmail.com>
2024-03-06 23:05:10 +0800

    style: Update body class in layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`${inter.className} dark:bg-dark-0 `}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
         </ThemeProvider>
