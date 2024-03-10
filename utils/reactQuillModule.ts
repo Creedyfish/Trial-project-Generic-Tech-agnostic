26acc0e86a466aea5394c6638fa84f74c645ec50
Creedyfish <ielbanbuenaofficial@gmail.com>
2024-03-10 15:47:49 +0800

    refactor: Refactor Quill editor configuration and formats

--- utils/reactQuillModule.ts
+++ utils/reactQuillModule.ts
@@ -1,42 +1,45 @@
+/**
+ * Configuration for the Quill editor toolbar.
+ * See https://quilljs.com/docs/modules/toolbar/ for more information.
+ */
 const modules = {
-    toolbar: [
-      [{ header: "1" }, { header: "2" }],
-      [{ size: [] }],
-      ["bold", "italic", "underline", "strike", "blockquote"],
-      [
-        { list: "ordered" },
-        { list: "bullet" },
-        { indent: "-1" },
-        { indent: "+1" },
-      ],
-      ["link", "image"],
-      ["clean"],
-    ],
-    clipboard: {
-      
-      matchVisual: false,
-    },
-  };
-  /*
-   * Quill editor formats
-   * See https://quilljs.com/docs/formats/
-   */
-  const formats = [
-    "header",
-    "font",
-    "size",
-    "bold",
-    "italic",
-    "underline",
-    "strike",
-    "blockquote",
-    "list",
-    "bullet",
-    "indent",
-    "link",
-    "image",
-    "video",
-  ];
+  toolbar: [
+    [{ header: "1" }, { header: "2" }], // Headers
+    [{ size: [] }], // Font sizes
+    ["bold", "italic", "underline", "strike", "blockquote"], // Styling options
+    [
+      { list: "ordered" },
+      { list: "bullet" },
+      { indent: "-1" },
+      { indent: "+1" },
+    ], // List and indentation options
+    ["link", "image"], // Link and image options
+    ["clean"], // Remove formatting
+  ],
+  clipboard: {
+    // Match visual while copy-pasting. Set to false for better formatting while pasting.
+    matchVisual: false,
+  },
+};
+
+/**
+ * Quill editor formats.
+ * These correspond to the options provided in the toolbar above.
+ * See https://quilljs.com/docs/formats/ for more information.
+ */
+const formats = [
+  "header",
+  "font",
+  "size",
+  "bold",
+  "italic",
+  "underline",
+  "strike",
+  "blockquote",
+  "list",
+  "bullet",
+  "indent",
+];
 
 
   export {modules,formats}
