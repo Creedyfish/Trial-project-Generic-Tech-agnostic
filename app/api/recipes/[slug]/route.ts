5f3e805d381e351d96723d3b81359e51866d895f
Creedyfish <ielbanbuenaofficial@gmail.com>
2024-03-10 14:11:06 +0800

    feat: Add DELETE route for recipes

--- app/api/recipes/[slug]/route.ts
+++ app/api/recipes/[slug]/route.ts
@@ -14,3 +14,17 @@ export async function GET(req: Request, res: NextResponse) {
     return NextResponse.json(error);
   }
 }
+
+export async function DELETE(req: Request, res: NextResponse) {
+  try {
+    const url = new URL(req.url);
+  const slug = url.pathname.split('/').slice(-1)[0];
+  const recipe = data.recipes.find(
+    (recipe) => recipe.name === slug.replace(/-/g, " ")
+  );
+
+  return NextResponse.json("Product Deleted");
+} catch (error) {
+  return NextResponse.json(error);
+}
+}
