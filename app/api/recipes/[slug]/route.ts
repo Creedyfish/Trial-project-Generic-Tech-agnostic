cf503908a207e68a6d359fa9ace8bcd3dcab3afa
Creedyfish <ielbanbuenaofficial@gmail.com>
2024-03-10 15:47:02 +0800

    refactor: Refactor recipe deletion logic in route.ts

--- app/api/recipes/[slug]/route.ts
+++ app/api/recipes/[slug]/route.ts
@@ -18,13 +18,20 @@ export async function GET(req: Request, res: NextResponse) {
 export async function DELETE(req: Request, res: NextResponse) {
   try {
     const url = new URL(req.url);
-  const slug = url.pathname.split('/').slice(-1)[0];
-  const recipe = data.recipes.find(
-    (recipe) => recipe.name === slug.replace(/-/g, " ")
-  );
+    const id = url.pathname.split('/').slice(-1)[0];
 
-  return NextResponse.json("Product Deleted");
-} catch (error) {
-  return NextResponse.json(error);
-}
+    // Find the index of the recipe with the given id
+    const index = data.recipes.findIndex((recipe) => Number(id) );
+
+    if (index !== -1) {
+      // Remove the recipe from the array
+      data.recipes.splice(index, 1);
+
+      return NextResponse.json("Recipe Deleted");
+    } else {
+      return NextResponse.json("Recipe not found");
+    }
+  } catch (error) {
+    return NextResponse.json(error);
+  }
 }
