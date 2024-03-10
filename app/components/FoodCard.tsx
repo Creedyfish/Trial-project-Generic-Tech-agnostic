63f838991dba0ab3d8ce200aaad093b122e29e3c
Creedyfish <ielbanbuenaofficial@gmail.com>
2024-03-10 09:45:06 +0800

    refactor: Refactor FoodCard component to improve code readability and performance

--- app/components/FoodCard.tsx
+++ app/components/FoodCard.tsx
@@ -1,35 +1,36 @@
+// Importing necessary libraries and data
 import React from "react";
-import data from "@/data.json";
 import Image from "next/image";
 
+// Defining the type of props that the FoodCard component will receive
 interface Props {
-  name: string;
-  image: string;
+  name: string; // Name of the food item
+  image: string; // Path to the image of the food item
 }
 
+// FoodCard component
 function FoodCard({ name, image }: Props) {
-  // const ingredientsHtml = data.recipes[0].procedure;
-  {
-    /* <div
-        className="ingredients font-lexend list-disc"
-        dangerouslySetInnerHTML={{ __html: ingredientsHtml }}
-      /> */
-  }
+  // The component doesn't have any internal state or side effects, so the body of the function is empty
+
+  // The component returns a div element with a specific style, containing an Image component and a div for the name
   return (
-    <div className="flex w-[20.3125rem] flex-col justify-center items-center gap-2">
-      <Image
-        src={`/${image}`}
-        width={325}
-        height={150}
-        alt={`${name}.image`}
-        loading="lazy"
-        className="w-[20.3125rem] h-[9.5rem] object-cover rounded-md"
-      />
+    <div className="flex w-[20.3125rem]  flex-col justify-center items-center gap-2">
+      <div className="overflow-hidden">
+        <Image
+          src={`/${image}`} // Image source is derived from the image prop
+          width={325}
+          height={150}
+          alt={`${name}.image`} // Alt text for the image is derived from the name prop
+          loading="lazy" // Image loading is set to lazy to improve performance
+          className="w-[20.3125rem] h-[9.5rem] object-cover group-hover:scale-110 rounded-md transition-all duration-300 ease-in-out" // Various CSS classes for styling
+        />
+      </div>
       <div className="font-lexend w-full font-semibold text-sm md:text-xl items-start border-l-4 border-secondary">
-        <div className="px-2">{name}</div>
+        <div className="px-2">{name}</div> // The name prop is displayed here
       </div>
     </div>
   );
 }
 
+// The FoodCard component is exported for use in other parts of the application
 export default FoodCard;
