dfd9c0c1ff6b9823e091ef0a3672007f16f4350c
Creedyfish <ielbanbuenaofficial@gmail.com>
2024-03-10 11:56:40 +0800

    feat: Add Link component and update styles in recipe edit page

--- app/recipe/edit/[slug]/page.tsx
+++ app/recipe/edit/[slug]/page.tsx
@@ -3,6 +3,7 @@ import Loading from "@/app/Loading";
 import React from "react";
 import Image from "next/image";
 import dynamic from "next/dynamic";
+import Link from "next/link";
 import { modules, formats } from "@/utils/reactQuillModule";
 import { SaveSuccessModal, CancelModal } from "@/app/components/Modals";
 import {
@@ -15,7 +16,6 @@ import { useState, useEffect } from "react";
 const ReactQuill = dynamic(() => import("react-quill"), { ssr: false });
 import "react-quill/dist/quill.snow.css";
 import { addRecipe, getRecipe } from "@/queries/apiQueries";
-import { DevTool } from "@hookform/devtools";
 
 type Recipe = {
   id: number;
@@ -115,7 +115,10 @@ function Page({ params }: { params: { slug: string } }) {
           <div className="w-full h-full flex flex-col justify-center items-center md:items-start md:px-24 md:pt-24 py-5 px-9 container transition-all duration-300 ease-in-out">
             <div className="w-full h-full md:w-2/3 lg:w-3/5 xl:w-1/2  transition-all duration-300 ease-in-out">
               <div className="flex flex-col gap-10 h-full w-full ">
-                <div className="flex gap-2 items-center font-semibold font-lexend text-sm transition-all duration-100 ease-in-out text-dark-0 dark:text-light-0">
+                <Link
+                  href={"/"}
+                  className="flex gap-2 items-center font-semibold font-lexend text-sm transition-all duration-100 ease-in-out text-dark-0 dark:text-light-0"
+                >
                   <svg
                     width="14"
                     height="16"
@@ -126,7 +129,7 @@ function Page({ params }: { params: { slug: string } }) {
                     <path d="M32.4297 14.5C32.4297 15.7656 31.4453 16.75 30.25 16.75H8.66406L16.0469 24.2031C16.9609 25.0469 16.9609 26.5234 16.0469 27.3672C15.625 27.7891 15.0625 28 14.5 28C13.8672 28 13.3047 27.7891 12.8828 27.3672L1.63281 16.1172C0.71875 15.2734 0.71875 13.7969 1.63281 12.9531L12.8828 1.70312C13.7266 0.789062 15.2031 0.789062 16.0469 1.70312C16.9609 2.54688 16.9609 4.02344 16.0469 4.86719L8.66406 12.25H30.25C31.4453 12.25 32.4297 13.3047 32.4297 14.5Z" />
                   </svg>
                   <div>Back to Feed</div>
-                </div>
+                </Link>
                 <div className="font-payton text-[2rem] hidden md:flex leading-[2.5rem] text-primary">
                   New Recipe
                 </div>
@@ -252,7 +255,7 @@ function Page({ params }: { params: { slug: string } }) {
                           modules={modules}
                           value={field.value}
                           onChange={field.onChange}
-                          className="bg-bg-input dark:bg-dark-1"
+                          className="w-full"
                         />
                       )}
                     />
@@ -272,7 +275,7 @@ function Page({ params }: { params: { slug: string } }) {
                           modules={modules}
                           value={field.value}
                           onChange={field.onChange}
-                          className="bg-bg-input dark:bg-dark-1 w-full "
+                          className=" w-full "
                         />
                       )}
                     />
@@ -281,7 +284,7 @@ function Page({ params }: { params: { slug: string } }) {
                     <button
                       type="button"
                       onClick={() => setCancelIsOpen(true)}
-                      className="w-full md:w-auto text-light-0 font-lexend text-sm md:text-base px-4 py-2 bg-primary rounded-full"
+                      className="w-full md:w-auto text-light-0 font-lexend text-sm md:text-base px-4 py-2  rounded-full"
                     >
                       Cancel Edit
                     </button>
