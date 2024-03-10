ccb899a6bdbc6a50b7289a7b897877579062e205
Creedyfish <ielbanbuenaofficial@gmail.com>
2024-03-10 14:11:19 +0800

    feat: Add dynamic linking and update CardList component

--- app/components/CardList.tsx
+++ app/components/CardList.tsx
@@ -1,7 +1,37 @@
 import React from "react";
+import Link from "next/link";
+import FoodCard from "./FoodCard";
 
-function CardList() {
-  return <div>CardList</div>;
+interface Recipe {
+  name: string;
+  main_image: string;
+  user_id: number;
+}
+
+function CardList({
+  data,
+  active,
+}: {
+  data: Recipe[] | undefined;
+  active: string;
+}) {
+  return (
+    <>
+      {data?.map((recipe: Recipe, key: number) => (
+        <>
+          <Link
+            href={`${
+              active === "owner" ? "/recipes" : ""
+            }/${recipe.name.replace(/\s/g, "-")}`}
+            key={key}
+            className="flex justify-center items-center group"
+          >
+            <FoodCard name={recipe.name} image={recipe.main_image} />
+          </Link>
+        </>
+      ))}
+    </>
+  );
 }
 
 export default CardList;
